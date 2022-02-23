f0613f2246c523fd6b21992f873dff06c941acca
thuanbach <ThuanBach@uregina.ca>
2022-02-23 10:24:03 -0600

    Add CORS middle ware to enalbe cross orign

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+var cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 const { Pool, Client } = require('pg');
 
